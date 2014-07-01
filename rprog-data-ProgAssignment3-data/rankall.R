785029c29c568c2c45a80f5ba84e6f6be9a15c3f
Jari Koister <jari@eightcut.com>
2014-06-30 22:54:33 -0700

    ok 8,9

--- rprog-data-ProgAssignment3-data/rankall.R
+++ rprog-data-ProgAssignment3-data/rankall.R
@@ -7,15 +7,20 @@ setwd("/Users/jkoister/miscdev/rclass/rprog-data-ProgAssignment3-data")
 
 #cStatus ... jamfor numerid med string nar jag skall plocka ur hospital
 val_outcomes<-c("heart attack","heart failure","pneumonia")
-val_states<-c("AL","AK","AZ","AR","CA","CO","CT","DE","DC","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","MD","MA","MI","MN","MS","MO","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY")
+#val_states<-c("AK","AL","AZ","AR","CA","CO","CT","DE","DC","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","MD","MA","MI","MN","MS","MO","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY")
+val_states<-c("AK","AL","AZ","AR","CA","CO","CT","DC","DE","FL","GA","HI","IA","ID","IL","IN","KS","KY","LA","MA","MD","ME","MI","MN","MO","MS","MT","NC","ND","NE","NH","NJ","NM","NV","NY","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VA","VI","VT","WA","WI","WV","WY")
 
 rankall <- function(outcome, num = "best") {
   allrank<-NULL
   for (is in val_states){
-    #rbind(allrank,rankhospital(is,outcome,num))
-    print(rankhospital(is,outcome,num))
+
+    tmp<-rankhospital(is,outcome,num)
+    allrank<-rbind(allrank,tmp)
   }
-  return(allrank)
+  df<-as.data.frame(allrank)
+  row.names(df)<-val_states
+  colnames(df)<-c("hospital","state")
+  return(df)
 }
 
 
@@ -46,22 +51,46 @@ rankhospital <- function(state, outcome, numi = "best") {
   
   # If num is 0, return all
   if(numi == "all") {
+    # Return all
     return(state_outcome_dfo)
   }
   if(numi == "best") {
+    # Return first
     return(c(state_outcome_dfo[1,]$Hospital.Name,state))
+    stop("Wrong outcome string")
   }
   if(numi == "worst") {
+    # Return last
     return(c(state_outcome_dfo[nrow(state_outcome_dfo),]$Hospital.Name,state))
   }
+  # Return number numi
   theone<-state_outcome_dfo[numi,]
-  return(c(theone$Hospital.Name,theone$State))
+  return(c(theone$Hospital.Name,theone$State)) 
+
+}
+
+
+get_best<-function(df){
+  mvalue<-min(df$num)
+  min.df <- df[df$num==mvalue,]
+  sort.df <- with(min.df,  min.df[order(min.df$Hospital.Name), ])
+  return(sort.df[1,])
+  stop("Wrong outcome string")
+}
+
+
+get_worst<-function(df){
+  mvalue<-max(df$num)
+  max.df <- df[df$num==mvalue,]
+  sort.df <- with(max.df,  max.df[order(max.df$Hospital.Name), ])
+  return(sort.df[1,])
+  stop("Wrong outcome string")
 }
 
 
 best<-function(state,outcome){
   
-  
+  mvalue<-max(df$num)
   mvalue<-min(state_outcome_dfn$num)
   best_outcomes<-get_best(state_outcome_dfn,mvalue)
   #best_outcomes<-get_best2(state_outcome_df,col_n,mvalue)
@@ -102,13 +131,6 @@ get_outcome_wnum<-function(df,outcome){
   stop("problem in get_outcome_wnum")
 }
 
-get_best<-function(df,mvalue){
-  
-  min.df <- df[df$num==mvalue,]
-  sort.df <- with(min.df,  min.df[order(min.df$Hospital.Name), ])
-  return(sort.df)
-  stop("Wrong outcome string")
-}
 
 valid_state<-function(state){
   return(state %in% val_states)
@@ -122,3 +144,53 @@ valid_outcome<-function(o){
 debug<-function(lable,value){
   cat(lable,value)
 }
+
+
+rankhospital2 <- function(state, outcome, numi = "best") {
+  ## Read outcome data
+  ##read outcome data
+  full_df <- read.csv("outcome-of-care-measures.csv", colClasses = "character",na.strings=c("Not Available"))
+  ## chect state and outcome are valid
+  if(!valid_state(state)){
+    stop("invalid state")
+  }
+  if(!valid_outcome(outcome)){
+    stop("invalid outcome")
+  }
+  # Get column name
+  col_n<-col_outcome(outcome)
+  # Remove NA, could ne done after column selection for better performance
+  clean_df<-na.omit(full_df)
+  
+  # Get outcome data
+  outcome_df<-clean_df[,c('Hospital.Name','State',col_n)]
+  state_outcome_df<-outcome_df[outcome_df$State==state,]
+  # Add numeric column with values, called num
+  state_outcome_dfn<-get_outcome_wnum(state_outcome_df,outcome)
+  #return(state_outcome_dfn)
+  state_outcome_dfo<-state_outcome_dfn[order(state_outcome_dfn$num,state_outcome_dfn$Hospital.Name),]
+  #state_outcome_dfo<-state_outcome_dfn[order(state_outcome_dfn$num),]
+  
+  # If num is 0, return all
+  if(numi == "all") {
+    # Return all
+    return(state_outcome_dfo)
+  }
+  if(numi == "best") {
+    # Return first
+    #return(c(state_outcome_dfo[1,]$Hospital.Name,state))
+    best=get_best(state_outcome_dfn)
+    return(c(best$Hospital.Name,state))
+    stop("Wrong outcome string")
+  }
+  if(numi == "worst") {
+    # Return last
+    wor=get_worst(state_outcome_dfn)
+    return(c(wor$Hospital.Name,state))
+  }
+  # Return number numi
+  theone<-state_outcome_dfo[numi,]
+  return(c(theone$Hospital.Name,theone$State)) 
+  
+}
+
